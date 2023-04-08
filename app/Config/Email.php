af7c9beb49345558848a3440af492c16e8e72bf6
Kosmas Mahendra <blindcozy@users.noreply.github.com>
2023-04-08 17:34:41 +0700

    Deploy Myth/Auth

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'no-reply@58vapehouse.com';
+    public string $fromName   = '58 Vapehouse';
     public string $recipients = '';
 
     /**
