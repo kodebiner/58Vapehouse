1bab281e0fe556891fc54c327b0d084f2be00953
-S3ME- <99646777+S3ME@users.noreply.github.com>
2023-04-10 18:04:03 +0700

    Update table user

---
+++ app/Database/Migrations/2023-04-10-174010_update_user_table.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class UpdateUsers extends Migration
+{
+    public function up()
+    {
+        $fields = [
+            'firstname'         => ['type' => 'varchar', 'constraint' => 255],
+            'lastname'          => ['type' => 'varchar', 'constraint' => 255],
+        ];
+        $this->forge->addColumn('users', $fields);
+    }
+
+    public function down()
+    {
+        
+    }
+}
