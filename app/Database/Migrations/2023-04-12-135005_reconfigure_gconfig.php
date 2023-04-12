e7973273f071fd2ebea50eb9eaa877817af28846
-S3ME- <99646777+S3ME@users.noreply.github.com>
2023-04-12 14:49:16 +0700

    Migrate Gconfig table

--- app/Database/Migrations/2023-04-12-135005_reconfigure_gconfig.php
+++ app/Database/Migrations/2023-04-12-135005_reconfigure_gconfig.php
@@ -9,13 +9,22 @@ class ConfigUpdate extends Migration
     public function up()
     {
         $fields = [
-            'id'         => ['type' => 'INT', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true, 'first' => true]
+            'id'                => ['type' => 'INT', 'constraint' => 11, 'unsigned' => true, 'null' => false, 'auto_increment' => true],
+            'poinvalue'         => ['type' => 'VARCHAR', 'constraint' => 255],
+            'poinorder'         => ['type' => 'VARCHAR', 'constraint' => 255],
+            'memberdisc'        => ['type' => 'VARCHAR', 'constraint' => 255],
+            'memberdisctype'    => ['type' => 'TINYINT', 'constraint' => 1],
+            'logo'              => ['type' => 'VARCHAR', 'constraint' => 255],
+            'bizname'           => ['type' => 'VARCHAR', 'constraint' => 255],
+            'ppn'               => ['type' => 'VARCHAR', 'constraint' => 255],
         ];
-        $this->forge->addColumn('gconfig', $fields);
+        $this->forge->addField($fields);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('gconfig', true);
     }
 
     public function down()
     {
-        
+        $this->forge->dropTable('gconfig', true);
     }
 }
