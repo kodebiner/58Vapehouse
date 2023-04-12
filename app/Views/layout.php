012f67d1ee5abc95a1603ba9836d105eddbd4171
-S3ME- <99646777+S3ME@users.noreply.github.com>
2023-04-12 23:01:59 +0700

    Update layout

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -7,27 +7,27 @@
         <title></title>
         <meta name="description" content="">
         <meta name="author" content="PT. Kodebiner Teknologi Indonesia">
-        <link rel="icon" href="favicon/favicon.ico">
-        <link rel="apple-touch-icon" sizes="16x16" href="favicon/apple-icon-16x16.png">
-        <link rel="apple-touch-icon" sizes="32x32" href="favicon/apple-icon-32x32.png">
-        <link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="96x96" href="favicon/apple-icon-96x96.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-icon-152x152.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-icon-180x180.png">
-        <link rel="apple-touch-icon" sizes="192x192" href="favicon/apple-icon-192x192.png">
-        <link rel="manifest" href="favicon/manifest.json">
+        <link rel="icon" href="<?=base_url();?>/favicon/favicon.ico">
+        <link rel="apple-touch-icon" sizes="16x16" href="<?=base_url();?>/favicon/apple-icon-16x16.png">
+        <link rel="apple-touch-icon" sizes="32x32" href="<?=base_url();?>/favicon/apple-icon-32x32.png">
+        <link rel="apple-touch-icon" sizes="57x57" href="<?=base_url();?>/favicon/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="<?=base_url();?>/favicon/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="<?=base_url();?>/favicon/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="<?=base_url();?>/favicon/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="96x96" href="<?=base_url();?>/favicon/apple-icon-96x96.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="<?=base_url();?>/favicon/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="<?=base_url();?>/favicon/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="<?=base_url();?>/favicon/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="<?=base_url();?>/favicon/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="<?=base_url();?>/favicon/apple-icon-180x180.png">
+        <link rel="apple-touch-icon" sizes="192x192" href="<?=base_url();?>/favicon/apple-icon-192x192.png">
+        <link rel="manifest" href="<?=base_url();?>/favicon/manifest.json">
         <meta name="msapplication-TileColor" content="#ffffff">
-        <meta name="msapplication-TileImage" content="favicon/ms-icon-144x144.png">
+        <meta name="msapplication-TileImage" content="<?=base_url();?>/favicon/ms-icon-144x144.png">
         <meta name="theme-color" content="#ffffff">
-        <link rel="stylesheet" href="css/theme.css">
-        <script src="js/uikit.min.js"></script>
-        <script src="js/uikit-icons.min.js"></script>
+        <link rel="stylesheet" href="<?=base_url();?>/css/theme.css">
+        <script src="<?=base_url();?>/js/uikit.min.js"></script>
+        <script src="<?=base_url();?>/js/uikit-icons.min.js"></script>
 
         <!-- Extra Script Section -->
         <?= $this->renderSection('extraScript') ?>
