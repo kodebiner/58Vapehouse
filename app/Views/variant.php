24f409f79cab9d2bd101d6b46b8f5732888b7805
Rizalramadhan1999 <ramadhanrizal30@gmail.com>
2023-05-05 08:47:44 +0700

    variant view

--- app/Views/variant.php
+++ app/Views/variant.php
@@ -93,36 +93,31 @@
     </thead>
     <tbody>
         <?php $i = 1 ; ?>
-        <?php foreach ($variants as $variant) { ?>
-          <tr>
-            <td class="uk-text-center"><?= $i++; ?></td>
-            <td class="uk-text-center"><?= $variant['name']; ?></td>
-            <td class="uk-text-center"><?= $variant['hargamodal'] ?></td>
-            <td class="uk-text-center"><?= $variant['hargadasar'] ?></td>
-            <td class="uk-text-center"><?= $variant['hargajual'] ?></td>
-            <td class="uk-text-center">
-              <?php foreach ($stock as $stok) { 
-                if ($stok['id'] === $variant['id']){
-                   echo $stok['qty'];
-                }
-                 } ?>
-            </td>
+        <?php foreach ($stock as $stok) : ?>
+          <?php foreach ($variants as $variant) { ?>
+            <tr>
+              <td class="uk-text-center"><?= $i++; ?></td>
+              <td class="uk-text-center"><?= $variant['name']; ?></td>
+              <td class="uk-text-center"><?= $variant['hargamodal'] ?></td>
+              <td class="uk-text-center"><?= $variant['hargadasar'] ?></td>
+              <td class="uk-text-center"><?= $variant['hargajual'] ?></td>
+              <td class="uk-text-center"><?= $stok['qty'] ?></td>
+              <td class="uk-child-width-auto uk-flex-center uk-grid-row-small uk-grid-column-small" uk-grid>
+                <!-- Button Trigger Modal Edit -->
+                <div>
+                  <button type="button" class="uk-button uk-button-primary" uk-toggle="target: #editdata<?= $variant['id'] ?>"><?=lang('Global.edit')?></button>
+                </div>
+                <!-- End Of Button Trigger Modal Edit -->
 
-            <td class="uk-child-width-auto uk-flex-center uk-grid-row-small uk-grid-column-small" uk-grid>
-              <!-- Button Trigger Modal Edit -->
-              <div>
-                <button type="button" class="uk-button uk-button-primary" uk-toggle="target: #editdata<?= $variant['id'] ?>"><?=lang('Global.edit')?></button>
-              </div>
-              <!-- End Of Button Trigger Modal Edit -->
-
-              <!-- Button Delete -->
-              <div>
-                <a class="uk-button uk-button-default uk-button-danger" href="product/deletevar/<?= $variant['id'] ?>" onclick="return confirm('<?=lang('Global.deleteConfirm')?>')"><?=lang('Global.delete')?></a>
-              </div>
-              <!-- End Of Button Delete -->
-            </td>
-          </tr>
-        <?php } ?>
+                <!-- Button Delete -->
+                <div>
+                  <a class="uk-button uk-button-default uk-button-danger" href="product/deletevar/<?= $variant['id'] ?>" onclick="return confirm('<?=lang('Global.deleteConfirm')?>')"><?=lang('Global.delete')?></a>
+                </div>
+                <!-- End Of Button Delete -->
+              </td>
+            </tr>
+          <?php  } ?>
+        <?php endforeach; ?>
     </tbody>
   </table>
 
