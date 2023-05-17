aae3afb7269640b1ae805bfadb2fafe2028613bd
-S3ME- <99646777+S3ME@users.noreply.github.com>
2023-05-17 15:10:32 +0700

    update view bundle

--- app/Views/bundle.php
+++ app/Views/bundle.php
@@ -10,161 +10,161 @@
 
 <!-- Page Heading -->
 <div class="tm-card-header uk-light">
-    <?= view('Views/Auth/_message_block') ?>
+  <?= view('Views/Auth/_message_block') ?>
 
-    <div uk-grid class="uk-flex-middle">
-        <div class="uk-width-1-2@m">
-            <h3 class="tm-h3"><?=lang('Global.bundleList')?></h3>
-        </div>
-
-        <!-- Button Trigger Modal Add -->
-        <div class="uk-width-1-2@m uk-text-right@m">
-            <button type="button" class="uk-button uk-button-primary uk-preserve-color" uk-toggle="target: #tambahdata"><?=lang('Global.addBundle')?></button>
-        </div>
-        <!-- End Of Button Trigger Modal Add -->
-
-        <!-- Modal Add -->
-        <div uk-modal class="uk-flex-top" id="tambahdata">
-            <div class="uk-modal-dialog uk-margin-auto-vertical">
-                <div class="uk-modal-content">
-                    <div class="uk-modal-header">
-                        <h5 class="uk-modal-title" id="tambahdata" ><?=lang('Global.addBundle')?></h5>
-                    </div>
-                    <div class="uk-modal-body">
-                        <form class="uk-form-stacked" role="form" action="bundle/create" method="post">
-                            <?= csrf_field() ?>
-
-                            <div class="uk-margin-bottom">
-                                <label class="uk-form-label" for="name"><?=lang('Global.name')?></label>
-                                <div class="uk-form-controls">
-                                    <input type="text" class="uk-input <?php if (session('errors.name')) : ?>tm-form-invalid<?php endif ?>" id="name" name="name" placeholder="<?=lang('Global.name')?>" required />
-                                </div>
-                            </div>
-
-                            <div class="uk-margin-bottom">
-                                <label class="uk-form-label" for="price"><?=lang('Global.price')?></label>
-                                <div class="uk-form-controls">
-                                    <input type="text" class="uk-input <?php if (session('errors.price')) : ?>tm-form-invalid<?php endif ?>" id="price" name="price" placeholder="<?=lang('Global.price')?>" required />
-                                </div>
-                            </div>
-
-                            <div id="createBundle" class="uk-margin-bottom">
-                                <h4 class="tm-h4 uk-margin-remove"><?=lang('Global.bundle')?></h4>
-                                <div class="uk-text-right">
-                                    <a onclick="createNewBundle()">+ Add More Bundle</a>
-                                </div>
-                                <?php
-                                $combProducts = [];
-                                foreach ($variants as $variant) {
-                                  foreach ($products as $product) {
-                                    if ($variant['productid'] === $product['id']) {
-                                      $combProducts[] = [$variant['id'] => $product['name'].' - '.$variant['name']];
-                                    }
-                                  }
-                                }
-                                ?>
-                                <div id="variantcontainer0" class="uk-margin-small" uk-grid>
-                                  <div class="uk-width-5-6">
-                                    <input class="uk-input" id="productvariantname0" required/>
-                                    <input id="variantid0" name="variantid[0]" hidden/>
-                                  </div>
-                                </div>
-                                <script type="text/javascript">
-                                  $(function() {
-                                    var combProduct = [
-                                      <?php
-                                        foreach ($combProducts as $combProduct) {
-                                          foreach ($combProduct as $key => $value) {
-                                            echo '{label:"'.$value.'", idx:'.$key.'},';
-                                          }
-                                        }
-                                      ?>
-                                    ];
-                                    $("#productvariantname0").autocomplete({
-                                      source: combProduct,
-                                      select: function(e, i) {
-                                        $('#variantid0').val(i.item.idx);
-                                      },
-                                      minLength: 2
-                                    });
-                                  });
-
-                                  var variantidx = 0;
-                                  function createNewBundle() {
-                                    variantidx ++;
-                                    const bundlecontainer = document.getElementById('createBundle');
-
-                                    const variantcontainer = document.createElement('div');
-                                    variantcontainer.setAttribute('id', 'variantcontainer'+variantidx);
-                                    variantcontainer.setAttribute('class', 'uk-margin-small');
-                                    variantcontainer.setAttribute('uk-grid', '');
-
-                                    const formcontainer = document.createElement('div');
-                                    formcontainer.setAttribute('class', 'uk-width-5-6');
-
-                                    const variantname = document.createElement('input');
-                                    variantname.setAttribute('id', 'productvariantname'+variantidx);
-                                    variantname.setAttribute('class', 'uk-input');
-
-                                    const variantid = document.createElement('input');
-                                    variantid.setAttribute('id', 'variantid'+variantidx);
-                                    variantid.setAttribute('name', 'variantid['+variantidx+']');
-                                    variantid.setAttribute('hidden', '');
-
-                                    const closecontainer = document.createElement('div');
-                                    closecontainer.setAttribute('class', 'uk-width-1-6 uk-flex uk-flex-middle');
-
-                                    const closebutton = document.createElement('a');
-                                    closebutton.setAttribute('class', 'uk-text-danger');
-                                    closebutton.setAttribute('onclick', 'removeVariant('+variantidx+')');
-                                    closebutton.setAttribute('uk-icon', 'close');
-
-                                    formcontainer.appendChild(variantname);
-                                    formcontainer.appendChild(variantid);
-                                    closecontainer.appendChild(closebutton);
-                                    variantcontainer.appendChild(formcontainer);
-                                    variantcontainer.appendChild(closecontainer);
-                                    bundlecontainer.appendChild(variantcontainer);
-
-                                    $(function() {
-                                      var combProductArr = [
-                                        <?php
-                                        foreach ($combProducts as $combProduct) {
-                                          foreach ($combProduct as $key => $value) {
-                                            echo '{label:"'.$value.'", idx:'.$key.'},';
-                                          }
-                                        }
-                                        ?>
-                                      ];
-                                      $("#productvariantname"+variantidx).autocomplete({
-                                        source: combProductArr,
-                                        select: function(e, i) {
-                                          $('#variantid'+variantidx).val(i.item.idx);
-                                        },
-                                        minLength: 2
-                                      });
-                                    });
-                                  };
-                                  
-                                  function removeVariant(i) {
-                                    variant = document.getElementById('variantcontainer'+i);
-                                    variant.remove();
-                                  }
-                                </script>
-                            </div>
-
-                            <hr>
-
-                            <div class="uk-margin">
-                                <button type="submit" class="uk-button uk-button-primary"><?=lang('Global.save')?></button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- End Of Modal Add -->
+  <div uk-grid class="uk-flex-middle">
+    <div class="uk-width-1-2@m">
+      <h3 class="tm-h3"><?=lang('Global.bundleList')?></h3>
     </div>
+
+    <!-- Button Trigger Modal Add -->
+    <div class="uk-width-1-2@m uk-text-right@m">
+      <button type="button" class="uk-button uk-button-primary uk-preserve-color" uk-toggle="target: #tambahdata"><?=lang('Global.addBundle')?></button>
+    </div>
+    <!-- End Of Button Trigger Modal Add -->
+
+    <!-- Modal Add -->
+    <div uk-modal class="uk-flex-top" id="tambahdata">
+      <div class="uk-modal-dialog uk-margin-auto-vertical">
+        <div class="uk-modal-content">
+          <div class="uk-modal-header">
+            <h5 class="uk-modal-title" id="tambahdata" ><?=lang('Global.addBundle')?></h5>
+          </div>
+          <div class="uk-modal-body">
+            <form class="uk-form-stacked" role="form" action="bundle/create" method="post">
+              <?= csrf_field() ?>
+
+              <div class="uk-margin-bottom">
+                <label class="uk-form-label" for="name"><?=lang('Global.name')?></label>
+                <div class="uk-form-controls">
+                  <input type="text" class="uk-input <?php if (session('errors.name')) : ?>tm-form-invalid<?php endif ?>" id="name" name="name" placeholder="<?=lang('Global.name')?>" required />
+                </div>
+              </div>
+
+              <div class="uk-margin-bottom">
+                <label class="uk-form-label" for="price"><?=lang('Global.price')?></label>
+                <div class="uk-form-controls">
+                  <input type="text" class="uk-input <?php if (session('errors.price')) : ?>tm-form-invalid<?php endif ?>" id="price" name="price" placeholder="<?=lang('Global.price')?>" required />
+                </div>
+              </div>
+
+              <div id="createBundle" class="uk-margin-bottom">
+                <h4 class="tm-h4 uk-margin-remove"><?=lang('Global.bundle')?></h4>
+                <div class="uk-text-right">
+                  <a onclick="createNewBundle()">+ Add More Bundle</a>
+                </div>
+                <?php
+                  $combProducts = [];
+                  foreach ($variants as $variant) {
+                    foreach ($products as $product) {
+                      if ($variant['productid'] === $product['id']) {
+                        $combProducts[] = [$variant['id'] => $product['name'].' - '.$variant['name']];
+                      }
+                    }
+                  }
+                ?>
+                <div id="variantcontainer0" class="uk-margin-small" uk-grid>
+                  <div class="uk-width-5-6">
+                    <input class="uk-input" id="productvariantname0" required/>
+                    <input id="variantid0" name="variantid[0]" hidden/>
+                  </div>
+                </div>
+                <script type="text/javascript">
+                  $(function() {
+                    var combProduct = [
+                      <?php
+                        foreach ($combProducts as $combProduct) {
+                          foreach ($combProduct as $key => $value) {
+                            echo '{label:"'.$value.'", idx:'.$key.'},';
+                          }
+                        }
+                      ?>
+                    ];
+                    $("#productvariantname0").autocomplete({
+                      source: combProduct,
+                      select: function(e, i) {
+                        $('#variantid0').val(i.item.idx);
+                      },
+                      minLength: 2
+                    });
+                  });
+
+                  var variantidx = 0;
+                  function createNewBundle() {
+                    variantidx ++;
+                    const bundlecontainer = document.getElementById('createBundle');
+
+                    const variantcontainer = document.createElement('div');
+                    variantcontainer.setAttribute('id', 'variantcontainer'+variantidx);
+                    variantcontainer.setAttribute('class', 'uk-margin-small');
+                    variantcontainer.setAttribute('uk-grid', '');
+
+                    const formcontainer = document.createElement('div');
+                    formcontainer.setAttribute('class', 'uk-width-5-6');
+
+                    const variantname = document.createElement('input');
+                    variantname.setAttribute('id', 'productvariantname'+variantidx);
+                    variantname.setAttribute('class', 'uk-input');
+
+                    const variantid = document.createElement('input');
+                    variantid.setAttribute('id', 'variantid'+variantidx);
+                    variantid.setAttribute('name', 'variantid['+variantidx+']');
+                    variantid.setAttribute('hidden', '');
+
+                    const closecontainer = document.createElement('div');
+                    closecontainer.setAttribute('class', 'uk-width-1-6 uk-flex uk-flex-middle');
+
+                    const closebutton = document.createElement('a');
+                    closebutton.setAttribute('class', 'uk-text-danger');
+                    closebutton.setAttribute('onclick', 'removeVariant('+variantidx+')');
+                    closebutton.setAttribute('uk-icon', 'close');
+
+                    formcontainer.appendChild(variantname);
+                    formcontainer.appendChild(variantid);
+                    closecontainer.appendChild(closebutton);
+                    variantcontainer.appendChild(formcontainer);
+                    variantcontainer.appendChild(closecontainer);
+                    bundlecontainer.appendChild(variantcontainer);
+
+                    $(function() {
+                      var combProductArr = [
+                        <?php
+                          foreach ($combProducts as $combProduct) {
+                            foreach ($combProduct as $key => $value) {
+                              echo '{label:"'.$value.'", idx:'.$key.'},';
+                            }
+                          }
+                        ?>
+                      ];
+                      $("#productvariantname"+variantidx).autocomplete({
+                        source: combProductArr,
+                        select: function(e, i) {
+                          $('#variantid'+variantidx).val(i.item.idx);
+                        },
+                        minLength: 2
+                      });
+                    });
+                  };
+                                  
+                  function removeVariant(i) {
+                    variant = document.getElementById('variantcontainer'+i);
+                    variant.remove();
+                  }
+                </script>
+              </div>
+
+              <hr>
+
+              <div class="uk-margin">
+                <button type="submit" class="uk-button uk-button-primary"><?=lang('Global.save')?></button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- End Of Modal Add -->
+  </div>
 </div>
 <!-- End Of Page Heading -->
 
