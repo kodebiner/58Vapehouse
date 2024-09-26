c078b848bc0aa186f46cd5f7f09e4555ccd7baa2
-S3ME- <99646777+S3ME@users.noreply.github.com>
2024-09-26 16:12:31 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -32,8 +32,10 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 
-$routes->get('/', 'Home::index', ['filter' => ['login', 'role:owner,supervisor,operator,guests']]);
-$routes->get('home/index', 'Home::index', ['filter' => ['login', 'role:owner,supervisor,operator,guests']]);
+$routes->get('/', 'Presence::index', ['filter' => ['login', 'role:owner,supervisor,operator,guests']]);
+$routes->get('home/index', 'Presence::index', ['filter' => ['login', 'role:owner,supervisor,operator,guests']]);
+// $routes->get('/', 'Home::index', ['filter' => ['login', 'role:owner,supervisor,operator,guests']]);
+// $routes->get('home/index', 'Home::index', ['filter' => ['login', 'role:owner,supervisor,operator,guests']]);
 $routes->get('trial', 'Home::trial');
 $routes->get('ownership', 'Home::ownership');
 // $routes->get('stockmovemigrate', 'Home::stockmove');
